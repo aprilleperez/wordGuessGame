d6447648054e278fbc1f97d1be299afceae51a96
aprilleperez <aprillesakura@gmail.com>
2019-03-01 18:07:04 -0800

    it's almost workinggit status

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,21 @@ var domLoses = document.getElementById("domLoses");
 var domGuessesRemaining = document.getElementById("domGuessesRemaining");
 var domIncorrectLetters = document.getElementById("domIncorrectLetters");
 
+var wins = 0;
+var loses = 0;
 
-// declare an array of words
-// app stores randomly chosen word
-var tunSongs = ["imagine", "needy", "nasa", "bloodline", "fakesmile", "badidea", "makeup", "ghostin", "inmyhead", "rings", "thankunext", "imbored"];
-var randomSong = tunSongs[Math.floor(Math.random() * tunSongs.length)];
+
+var tunSongs = ["imagine", "needy", "nasa", "bloodline", "fakesmile", "badidea", "makeup", "ghostin", "inmyhead", "rings", "thankunext", "imbored"]; // declare an array of words
+var randomSong = tunSongs[Math.floor(Math.random() * tunSongs.length)]; // app stores randomly chosen word as alphabet
 console.log(randomSong);
 
-// declare array storing indexes of chosen word's letters
-var chosenSong = [];
+
+var chosenSong = []; // declare array storing indexes of chosen word's letters as underscores
+
+
+var incorrectLetter = []; // store letters that don't match chosenWord
+var guessesRemaining = randomSong.length; // declare starting counter
+
 
 // loop through chosen word and create matching underscores for its length
 for (var i = 0; i < randomSong.length; i++) {
@@ -22,25 +28,152 @@ for (var i = 0; i < randomSong.length; i++) {
 }
 console.log(chosenSong);
 
-// store letters that don't match chosenWord
-var incorrectLetter = [];
-var guessesRemaining = 10;
 
 
+// --------------------------------------------START OF GAME LOOP triggered by pressing key---------------------------------------
 
-// listen for letter pressed
-// store myLetter pressed 
-// loop through index of chosen word
-// if myLetter matches indexed letter
-// change chosenSong[i] = myLetter
-// else myLetter doesn't match indexed chosenSong
-// push myLetter to incorrectLetter
-document.onkeydown = function (event) {
-    var myLetter = event.key;
+document.onkeyup = function (event) {
+    var myLetter = event.key; // store pressed key in myLetter (string)
     // console.log(myLetter)
+    // console.log(randomSong.indexOf(myLetter));
 
-    console.log(randomSong.indexOf(myLetter));
+    var intOfMyLetter = randomSong.indexOf(myLetter); // stores value of myLetter dependent on if indexed in randomSong
+    console.log(intOfMyLetter + "intofmyletter");
 
+    // if my letter is found in the index of randomSong
+    if (intOfMyLetter >= 0) { // if my letter is inside the word
+        //add loop through array for multiple letters -----------------CASE FOR MULTIPLE CORRECT LETTERS
+        for (var j = 0; j < randomSong.length; j++) {
+            if (randomSong[j] === myLetter) { //if myLetter(s) is found in any index of randomSong(s)
+                chosenSong[j] = myLetter       // matching indexes of chosenSong(underscores) change to myLetter
+            }
+        }
+
+        chosenSong.splice(intOfMyLetter, 1, myLetter);
+        var strSong = chosenSong.join("");
+        console.log(chosenSong + "win condition");
+        domChosenLetters.textContent = strSong;
+
+        if (strSong === randomSong) {
+            // alert("You won! Wanna keep playing?");
+            wins++;
+            domWins.textContent = wins;
+            // resetGame();
+        }
+
+    } else if (myLetter != undefined) { //as long as letter doesn't equal undefined
+        incorrectLetter.push(myLetter);
+        domIncorrectLetters.textContent += myLetter + " ";
+        guessesRemaining--; //decreases guessesRemaining only if guess is wrong
+        domGuessesRemaining.textContent = guessesRemaining;
+
+        if (guessesRemaining === 0) { // check for loss
+            alert("You lost :( Try Again?"); // end game
+            loses++;
+            domLoses.textContent = loses;
+            resetGame();
+        }
+    }
+}
+
+
+//WINS
+//if chosenSong (all underscores at first) === randomSong (string) <<< !!!!
+//alert "You won! Wanna keep playing?"
+//increment win
+//change textContent to added win value
+//call resetGame
+
+
+// function win() {
+//     if (chosenSong === randomSong) {
+//         wins++;
+//         alert("You won! Click Reset button to keep playing.");
+//         domWins.textContent = wins;
+//     }   
+// }
+
+
+
+
+// --------------------------------------------START OF RESET triggered by win/lose of game---------------------------------------
+//reset song, incorrect letters, and 0 out guessesRemaining
+function resetGame() {
+    incorrectLetter = []; // 0s out incorrect letters
+    domIncorrectLetters.textContent = incorrectLetter; // prints it to screen
+    randomSong = tunSongs[Math.floor(Math.random() * tunSongs.length)] // randomizes song again
+    console.log(randomSong);
+    guessesRemaining = randomSong.length; // sets guesses remaining to new song length
+    domGuessesRemaining.textContent = guessesRemaining;
+    chosenSong = [];
+    for (var i = 0; i < randomSong.length; i++) {
+        chosenSong[i] = "_ ";
+        domChosenLetters.textContent += chosenSong[i];
+    }
+
+    domChosenLetters.textContent = chosenSong.join("");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CREATE README AND ADD TO PORTFOLIO
+
+
+
+
+
+
+
+
+
+
+
+
+// updates word by replacing underscores with letter guessed
+// function correctLetter() {
+//     document.getElementById("domChosenLetters").textContent = "";
+//     for (var j = 0; j < chosenSong.randomSong.length; j++) {
+//         document.getElementById("domChosenLetters").textContent += chosenSong.myLetter[j];
+//     }
+// }
+
+
+
+    // for (var j = 0; j < chosenSong.length; j++) {
+    //     if (myLetter === chosenSong[j]) {
+    //         chosenSong[j] = myLetter;
+    //     } else {
+    //         incorrectLetter.push(myLetter);
+    //         domIncorrectLetters.textContent += myLetter;
+    //     }
+    // }  
+
+
+
+        // listen for letter pressed
+    // store myLetter pressed 
+    // loop through index of chosen word
+    // if myLetter matches indexed letter
+    // change chosenSong[i] = myLetter
+    // else myLetter doesn't match indexed chosenSong
+    // push myLetter to incorrectLetter
     // OLD CODE THAT SORTA WORKED 
     // for (var j = 0; j < chosenSong.length; j++) {
     //     if (myLetter === chosenSong[j]) {
@@ -51,36 +184,6 @@ document.onkeydown = function (event) {
     //     }
     // } 
 
-    // for (var j = 0; j < chosenSong.length; j++) {
-    //     if (myLetter === chosenSong[j]) {
-    //         chosenSong[j] = myLetter;
-    //     } else {
-    //         incorrectLetter.push(myLetter);
-    //         domIncorrectLetters.textContent += myLetter;
-    //     }
-    // }  
-
-    var int = randomSong.indexOf(myLetter);
-
-
-    // if my letter is found in the index of chosenSong
-    if (int >= 0) { // if my letter is inside the word
-        chosenSong[int] = myLetter;
-        // domChosenLetters.textContent += myLetter;
-
-        
-    } else if (myLetter != undefined) {
-        incorrectLetter.push(myLetter);
-        domIncorrectLetters.textContent += myLetter + " ";
-        guessesRemaining--; //decreases guessesRemaining only if guess is wrong
-        domGuessesRemaining.textContent = guessesRemaining;
-        //check for loss 
-        if (guessesRemaining === 0) {
-            // end game
-            alert("You lost :( Refresh to play again!");
-        }
-    }
-}
 
 
     // for (var j = 0; j < chosenSong.length; j++) {
@@ -90,7 +193,7 @@ document.onkeydown = function (event) {
     //         incorrectLetter.push(myLetter);
     //         domIncorrectLetters.textContent += myLetter;
     //     }
-    // }  
+    // } 
 
 
 
@@ -104,26 +207,27 @@ document.onkeydown = function (event) {
 
 
 
+    // if statement game loop old code
+   // chosenSong[intOfMyLetter] === myLetter; // indexed underscore will be my letter
+        // domChosenLetters.textContent = myLetter[intOfMyLetter];
+        // domChosenLetters.textContent = chosenSongmy[int];
+        // domChosenLetters.textContent += myLetter;
 
 
-// if myLetter ends up incorrect, go to incorrectLetter and decrease guessesRemaining
 
+
+//LOSES
 //if guessesRemaining === 0
+//add 1 point to lose
+//alert "you lost. press the reset button to keep playing. Exit game to stop"
 
+// function loses () {
+//     if (guessesRemaining === 0) {
+//         domLoses++;
+//         alert("You lost :( Click Reset button to keep playing.");
+//     }
 
-
-
-
-
-
-
-//LATER var remainingLetters = songs.length;
-
-
-
-
-
-
+// }
 
 
 
